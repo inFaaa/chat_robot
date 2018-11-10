89a86cfa1573beb409d54bb36664e8a3b1510b98
root <root@localhost.localdomain>
2018-11-10 22:28:22 +0800

    hello world

--- hello.py
+++ hello.py
@@ -2,8 +2,10 @@ import werobot
 
 robot = werobot.WeRoBot(token='tokenhere')
 
-@robot.text
+@robot.handler
 def hello_world():
     return 'Hello World!'
 
+robot.config['HOST'] = '0.0.0.0'
+robot.config['PORT'] = 80
 robot.run()

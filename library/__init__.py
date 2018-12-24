61122eddfc30213d1192ea056ac1a84d2bf236fb
Yongcong Lin <646677472@qq.com>
2018-12-24 22:50:03 +0800

    feat: simple query

--- library/__init__.py
+++ library/__init__.py
@@ -12,10 +12,10 @@ robot = WeRoBot(token='tokenhere')
 @robot.handler
 def processer(message):
     from util import get_entity, get_target_sentenses_index
-    entitys= get_entity(message.content)
-    entitys = ' '.join(entitys)#数据库中以空格划分
+    # entitys= get_entity(message.content)
+    # entitys = ' '.join(entitys)#数据库中以空格划分
     from library.knowledge.models import Knowledge
-    all_results = Knowledge.query.filter(Knowledge.k_entity == entitys).all()#这里还要针对查询进行改进
+    all_results = Knowledge.query.all()#这里还要针对查询进行改进
     indexs_weight_pair = get_target_sentenses_index(message.content,[i.k_title for i in all_results])
     answers = []
     for i in range(len(indexs_weight_pair)):
@@ -47,7 +47,7 @@ api = Api(app)
 # api.add_resource(library.question.resources.QueryQuestionByID, '/api/quesiton/<string: q_id>')
 # api.add_resource(library.question.resources.QueryQuestionByTitle, '/api/quesiton/<string: q_title>')
 api.add_resource(library.question.resources.QueryAllQuestion, '/api/quesiton/all')
-# api.add_resource(library.knowledge.resources.QueryKnowledgeByID, '/api/knowledge/<string: k_id>')
+api.add_resource(library.knowledge.resources.QueryKnowledge, '/api/knowledge')
 # api.add_resource(library.knowledge.resources.QueryKnowledgeByTitle, '/api/knowledge/<string: k_title>')
 api.add_resource(library.knowledge.resources.QueryAllKnowledge, '/api/knowledge/all')
 
